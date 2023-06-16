1b2f4c8b88d9dce1f3a95e7d9d3fdb2ce68ece8d
neiloy <neiloyhassan1@gmail.com>
2023-06-16 17:36:09 +0600

    frontend template

--- allStackToDo/client/src/App.js
+++ allStackToDo/client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome, User</h1>
+      <h4>Here are your tasks</h4>
+
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">Get the bread</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
     </div>
   );
 }
