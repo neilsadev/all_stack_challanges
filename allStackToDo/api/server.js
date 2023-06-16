ff66607ab7901c360313cf6860089017e9828de8
neiloy <neiloyhassan1@gmail.com>
2023-06-16 18:03:52 +0600

    frontend update

--- allStackToDo/api/server.js
+++ allStackToDo/api/server.js
@@ -1,17 +1,17 @@
-import express, { json } from "express";
-import mongoose, { connect } from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from "express"; // Importing the express module
+import mongoose from "mongoose"; // Importing the mongoose module
+import cors from "cors"; // Importing the cors module
+import dotenv from "dotenv"; // Importing the dotenv module
 
-import Todo from "./models/todo.js";
+import Todo from "./models/todo.js"; // Importing the Todo model
 
-dotenv.config(); // Load environment variables from .env file
-const app = express();
+dotenv.config(); // Loading environment variables from .env file
 
-app.use(json());
-app.use(cors());
+const app = express(); // Creating an instance of the Express application
+app.use(express.json()); // Middleware to parse request body as JSON
+app.use(cors()); // Enable Cross-Origin Resource Sharing
 
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 6001; // Setting the port for the server to listen on
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -19,55 +19,94 @@ mongoose
     useUnifiedTopology: true, // Use the new server discovery and monitoring engine
   })
   .then(() => {
-    // Start the server and listen on the specified port
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // Connection to MongoDB successful
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`); // Start the server and listen on the specified port
+    });
   })
-  .catch((error) => console.log(`${error} did not connect`));
-
-app.get("/todos", async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
-});
-
-app.post("/todo/new", (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
+  .catch((error) => {
+    console.error("MongoDB connection error:", error); // Log the MongoDB connection error
+    process.exit(1); // Terminate the application if the connection fails
   });
 
-  todo
-    .save()
-    .then((savedTodo) => {
-      res.json(savedTodo);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "An error occurred while saving the todo.",
-        error: error,
-      });
-    });
-});
-
-app.delete("/todo/delete/:id", async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id);
-
-  res.json(result);
-});
-
-app.delete("/todo/delete", async (req, res) => {
+// Get all todos
+app.get("/todos", async (req, res) => {
   try {
-    const result = await Todo.deleteMany({});
-    res.json(result);
+    const todos = await Todo.find(); // Fetch all todos from the database
+    res.json(todos); // Send the todos as a JSON response
   } catch (error) {
-    res.status(500).json({ error: "An error occurred while deleting todos." });
+    console.error("Error fetching todos:", error); // Log the error if fetching todos fails
+    res.status(500).json({
+      message: "An error occurred while fetching todos.", // Send an error response with a descriptive message
+      error: error.message,
+    });
   }
 });
 
-app.put("/todo/complete/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
+// Create a new todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { text } = req.body; // Extract the todo text from the request body
+    const todo = new Todo({ text }); // Create a new todo instance
+    const savedTodo = await todo.save(); // Save the todo to the database
+    res.json(savedTodo); // Send the saved todo as a JSON response
+  } catch (error) {
+    console.error("Error saving todo:", error); // Log the error if saving todo fails
+    res.status(500).json({
+      message: "An error occurred while saving the todo.", // Send an error response with a descriptive message
+      error: error.message,
+    });
+  }
+});
 
-  todo.complete = !todo.complete;
-  todo.save();
-  res.json(todo);
+// Delete a todo by ID
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // Extract the todo ID from the request parameters
+    const result = await Todo.findByIdAndDelete(id); // Find and delete the todo by ID
+    res.json(result); // Send the deletion result as a JSON response
+  } catch (error) {
+    console.error("Error deleting todo:", error); // Log the error if deleting todo fails
+    res.status(500).json({
+      message: "An error occurred while deleting the todo.", // Send an error response with a descriptive message
+      error: error.message,
+    });
+  }
+});
 
-  res.json(result);
+// Delete all todos
+app.delete("/todos", async (req, res) => {
+  try {
+    const result = await Todo.deleteMany({}); // Delete all todos from the database
+    res.json(result); // Send the deletion result as a JSON response
+  } catch (error) {
+    console.error("Error deleting todos:", error); // Log the error if deleting todos fails
+    res.status(500).json({
+      message: "An error occurred while deleting todos.", // Send an error response with a descriptive message
+      error: error.message,
+    });
+  }
+});
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // Get the id parameter from the request URL
+    const todo = await Todo.findById(id); // Find the todo by id using Mongoose
+
+    if (!todo) {
+      // If todo is not found, return a 404 response with an error message
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    todo.complete = !todo.complete; // Toggle the 'complete' property of the todo
+    await todo.save(); // Save the updated todo to the database
+    res.json(todo); // Return the updated todo as a JSON response
+  } catch (error) {
+    console.error("Error updating todo:", error); // Log the error to the console
+    res.status(500).json({
+      // Return a 500 response with an error message
+      message: "An error occurred while updating the todo.",
+      error: error.message,
+    });
+  }
 });
--- allStackToDo/client/src/App.js
+++ allStackToDo/client/src/App.js
@@ -1,16 +1,152 @@
+import { useEffect, useState } from "react";
+
+const apiBase = "http://localhost:3001";
+
 function App() {
+  // State variables
+  const [todos, setTodos] = useState([]); // Holds the list of todos
+  const [popupActive, setPopupActive] = useState(false); // Controls the visibility of the popup
+  const [newTodo, setNewTodo] = useState(""); // Holds the text for a new todo
+
+  useEffect(() => {
+    // Fetch todos when the component mounts
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    // Fetch todos from the API
+    fetch(apiBase + "/todos")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch todos");
+        }
+        return res.json();
+      })
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error:", err.message));
+  };
+
+  const completeTodo = async (id) => {
+    try {
+      // Update the completion status of a todo
+      const response = await fetch(apiBase + "/todos/" + id, {
+        method: "PUT",
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to complete todo");
+      }
+
+      const data = await response.json();
+
+      setTodos((todos) =>
+        todos.map((todo) => {
+          if (todo._id === data._id) {
+            todo.complete = data.complete;
+          }
+          return todo;
+        })
+      );
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+  const addTodo = async () => {
+    try {
+      // Add a new todo
+      const response = await fetch(apiBase + "/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: newTodo,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to add todo");
+      }
+
+      const data = await response.json();
+
+      setTodos([...todos, data]);
+      setPopupActive(false);
+      setNewTodo("");
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      // Delete a todo
+      const response = await fetch(apiBase + "/todos/" + id, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Delete request failed");
+      }
+
+      const data = await response.json();
+
+      setTodos((todos) => todos.filter((todo) => todo._id !== data?._id));
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+      // Handle the error as needed
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Welcome, User</h1>
-      <h4>Here are your tasks</h4>
+      <h1>Welcome, Tyler</h1>
+      <h4>Your tasks</h4>
 
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
-          <div className="text">Get the bread</div>
-          <div className="delete-todo">x</div>
-        </div>
+        {todos.length > 0 ? (
+          todos.map((todo) => (
+            <div
+              className={"todo" + (todo.complete ? " is-complete" : "")}
+              key={todo._id}
+              onClick={() => completeTodo(todo._id)}
+            >
+              <div className="checkbox"></div>
+              <div className="text">{todo.text}</div>
+              <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+                x
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>You currently have no tasks</p>
+        )}
       </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+
+      {popupActive && (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            X
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
